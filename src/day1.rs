76c0a3aee323f695dc79f9911a4f2c8471648d09
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-11-30 07:47:16 +0100

    day 1: solve part 2 day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,26 @@
+use std::collections::HashMap;
 use std::io::{self, Read};
 
-fn part_one(xs: Vec<i32>) {
-    let mut a: Vec<i32> = xs.iter().step_by(2).map(|x| *x).collect();
-    let mut b: Vec<i32> = xs.iter().skip(1).step_by(2).map(|x| *x).collect();
-    a.sort();
-    b.sort();
+fn part_one(a: &Vec<i32>, b: &Vec<i32>) {
     let mut result = 0;
     for i in 0..a.len() {
         result += (a[i] - b[i]).abs();
     }
-    println!("{:?}", result);
+    println!("{}", result);
 }
+
+fn part_two(a: Vec<i32>, b: Vec<i32>) {
+    let mut hm = HashMap::<i32, i32>::new();
+    for i in 0..b.len() {
+        *hm.entry(b[i]).or_insert(0) += 1;
+    }
+    let mut result = 0;
+    for i in 0..a.len() {
+        result += a[i] * hm.get(&a[i]).unwrap_or(&0);
+    }
+    println!("{}", result);
+}
+
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
@@ -18,5 +28,10 @@ pub fn solve() {
         .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
-    part_one(xs);
+    let mut a: Vec<i32> = xs.iter().step_by(2).map(|x| *x).collect();
+    let mut b: Vec<i32> = xs.iter().skip(1).step_by(2).map(|x| *x).collect();
+    a.sort();
+    b.sort();
+    part_one(&a, &b);
+    part_two(a, b);
 }
