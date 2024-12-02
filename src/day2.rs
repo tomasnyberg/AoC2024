3e3f67cafff26d3abda3c34d827d658c9aba4231
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-02 09:13:16 +0100

    day 2: solve part one (not super fast)

--- src/day2.rs
+++ src/day2.rs
@@ -3,5 +3,21 @@ use std::io::{self, Read};
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    println!("hej hopp");
+    let xss: Vec<Vec<i32>> = input
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| {
+            x.split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>();
+    let mut result = 0;
+    xss.iter().for_each(|xs| {
+        let increasing: bool = xs.windows(2).all(|x| x[0] < x[1]);
+        let decreasing: bool = xs.windows(2).all(|x| x[0] > x[1]);
+        let max_d: bool = xs.windows(2).all(|x| (x[0] - x[1]).abs() <= 3);
+        result += ((increasing || decreasing) && max_d) as i32;
+    });
+    println!("{}", result);
 }
