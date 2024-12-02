77672aadbdf568cc356bc091b0c8ea9687f94c10
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-02 09:15:13 +0100

    day 2: refactor out part one method

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,16 @@
 use std::io::{self, Read};
 
+pub fn part_one(xss: Vec<Vec<i32>>) -> i32 {
+    xss.iter()
+        .map(|xs| {
+            let increasing: bool = xs.windows(2).all(|x| x[0] < x[1]);
+            let decreasing: bool = xs.windows(2).all(|x| x[0] > x[1]);
+            let max_d: bool = xs.windows(2).all(|x| (x[0] - x[1]).abs() <= 3);
+            ((increasing || decreasing) && max_d) as i32
+        })
+        .sum()
+}
+
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
@@ -12,12 +23,5 @@ pub fn solve() {
                 .collect::<Vec<i32>>()
         })
         .collect::<Vec<Vec<i32>>>();
-    let mut result = 0;
-    xss.iter().for_each(|xs| {
-        let increasing: bool = xs.windows(2).all(|x| x[0] < x[1]);
-        let decreasing: bool = xs.windows(2).all(|x| x[0] > x[1]);
-        let max_d: bool = xs.windows(2).all(|x| (x[0] - x[1]).abs() <= 3);
-        result += ((increasing || decreasing) && max_d) as i32;
-    });
-    println!("{}", result);
+    println!("{}", part_one(xss));
 }
