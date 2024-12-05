2ca5c066001505f1f906818bac8206f1e6905b1a
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-05 09:00:00 +0100

    day 5: solve part 1

--- src/day5.rs
+++ src/day5.rs
@@ -8,5 +8,35 @@ fn part_two() {}
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    println!("hej hopp");
+
+    let parts: Vec<&str> = input.split("\n\n").collect();
+    let edges: Vec<&str> = parts[0].lines().collect();
+    let lists: Vec<Vec<i32>> = parts[1]
+        .lines()
+        .map(|line| line.split(',').map(|n| n.parse::<i32>().unwrap()).collect())
+        .collect();
+    let mut rules: HashMap<i32, Vec<i32>> = HashMap::new();
+    edges.iter().for_each(|edge| {
+        let mut parts = edge.split('|');
+        let a: i32 = parts.next().unwrap().parse::<i32>().unwrap();
+        let b: i32 = parts.next().unwrap().parse::<i32>().unwrap();
+        rules.entry(a).or_default().push(b);
+    });
+    let mut result = 0;
+    for list in lists {
+        let indices: HashMap<i32, usize> = list.iter().enumerate().map(|(i, &v)| (v, i)).collect();
+        let mut flag = false;
+        list.iter().enumerate().for_each(|(i, &v)| {
+            for y in rules.get(&v).unwrap_or(&vec![]) {
+                if indices.get(y).unwrap_or(&1000) < &i {
+                    flag = true;
+                    break;
+                }
+            }
+        });
+        if !flag {
+            result += list[list.len() / 2];
+        }
+    }
+    println!("{:?}", result);
 }
