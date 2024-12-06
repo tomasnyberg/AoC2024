caa341146c1fb35488a14b49c59ffc45002aea97
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-06 11:56:50 +0100

    day 6: optimize using jump table to quickly traverse known paths. 300ms -> < 100ms

--- src/day6.rs
+++ src/day6.rs
@@ -1,4 +1,5 @@
 use rayon::prelude::*;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io::{self, Read};
 
@@ -20,7 +21,7 @@ fn _debug_print(matrix: &[Vec<char>], t_i: i32, t_j: i32, dir_idx: usize) {
 }
 
 fn part_one(matrix: &[Vec<char>], t_i: i32, t_j: i32) -> HashSet<(i32, i32)> {
-    let result = find_loop(matrix, t_i, t_j);
+    let result = find_loop(matrix, t_i, t_j, -1, -1, true, &HashMap::new());
     let mut visited_two: HashSet<(i32, i32)> = HashSet::new();
     for (i, j, _) in result.1 {
         visited_two.insert((i, j));
@@ -28,16 +29,92 @@ fn part_one(matrix: &[Vec<char>], t_i: i32, t_j: i32) -> HashSet<(i32, i32)> {
     visited_two
 }
 
+fn insert_all(
+    result: &mut HashMap<(i32, i32, usize), (i32, i32, usize)>,
+    i: i32,
+    j: i32,
+    dir_idx: usize,
+    curr: &mut Vec<(i32, i32)>,
+) {
+    curr.iter().for_each(|&(ci, cj)| {
+        result.insert((ci, cj, dir_idx), (i, j, (dir_idx + 1) % 4));
+    });
+    curr.clear();
+}
+
+fn create_jump_table(matrix: &[Vec<char>]) -> HashMap<(i32, i32, usize), (i32, i32, usize)> {
+    let mut result: HashMap<(i32, i32, usize), (i32, i32, usize)> = HashMap::new();
+    let mut curr: Vec<(i32, i32)> = vec![];
+    // >
+    for i in 0..matrix.len() {
+        for j in 0..matrix[0].len() {
+            if matrix[i][j] == '#' {
+                insert_all(&mut result, i as i32, j as i32 - 1, 1, &mut curr);
+            } else {
+                curr.push((i as i32, j as i32));
+            }
+        }
+        insert_all(&mut result, -1, -1, 5, &mut curr);
+    }
+    // <
+    for i in 0..matrix.len() {
+        for j in (0..matrix[0].len()).rev() {
+            if matrix[i][j] == '#' {
+                insert_all(&mut result, i as i32, j as i32 + 1, 3, &mut curr);
+            } else {
+                curr.push((i as i32, j as i32));
+            }
+        }
+        insert_all(&mut result, -1, -1, 5, &mut curr);
+    }
+    // v
+    for j in 0..matrix[0].len() {
+        (0..matrix.len()).for_each(|i| {
+            if matrix[i][j] == '#' {
+                insert_all(&mut result, i as i32 - 1, j as i32, 2, &mut curr);
+            } else {
+                curr.push((i as i32, j as i32));
+            }
+        });
+        insert_all(&mut result, -1, -1, 5, &mut curr);
+    }
+    // ^
+    for j in 0..matrix[0].len() {
+        (0..matrix.len()).rev().for_each(|i| {
+            if matrix[i][j] == '#' {
+                insert_all(&mut result, i as i32 + 1, j as i32, 0, &mut curr);
+            } else {
+                curr.push((i as i32, j as i32));
+            }
+        });
+        insert_all(&mut result, -1, -1, 5, &mut curr);
+    }
+    result
+}
+
 fn find_loop(
     matrix: &[Vec<char>],
     mut t_i: i32,
     mut t_j: i32,
+    obs_i: i32,
+    obs_j: i32,
+    part_one: bool,
+    jump_table: &HashMap<(i32, i32, usize), (i32, i32, usize)>,
 ) -> (bool, HashSet<(i32, i32, usize)>) {
     let n = matrix.len() as i32;
     let m = matrix[0].len() as i32;
     let mut visited_three: HashSet<(i32, i32, usize)> = HashSet::new();
     let mut dir_idx: usize = 0;
     loop {
+        if !part_one && t_i != obs_i && t_j != obs_j {
+            let next_pos = jump_table.get(&(t_i, t_j, dir_idx)).unwrap_or(&(-1, -1, 0));
+            if next_pos.0 == -1 {
+                return (false, visited_three);
+            }
+            t_i = next_pos.0;
+            t_j = next_pos.1;
+            dir_idx = next_pos.2;
+        }
         if visited_three.contains(&(t_i, t_j, dir_idx)) {
             return (true, visited_three);
         }
@@ -67,12 +144,13 @@ fn part_two(
     for row in matrix {
         matrix_copy.push(row.clone());
     }
+    let jump_table = create_jump_table(matrix);
     part_one_visited
         .par_iter()
         .map(|&(i, j)| {
             let mut local_matrix = matrix_copy.clone();
             local_matrix[i as usize][j as usize] = '#';
-            let result = find_loop(&local_matrix, t_i, t_j);
+            let result = find_loop(&local_matrix, t_i, t_j, i, j, false, &jump_table);
             if result.0 {
                 1
             } else {
