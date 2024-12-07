75e35b6fb131be84d3e84b1257d71173172c3a07
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-07 11:43:15 +0100

    day 7: solve part 2

--- src/day7.rs
+++ src/day7.rs
@@ -5,7 +5,7 @@ fn part_one(xss: &[Vec<i64>], targets: &[i64]) -> i64 {
         .iter()
         .zip(xss.iter())
         .map(|(target, xs)| {
-            if dfs(*target, 1, xs, xs[0]) {
+            if dfs(*target, 1, xs, xs[0], true) {
                 return target;
             }
             &0
@@ -13,18 +13,42 @@ fn part_one(xss: &[Vec<i64>], targets: &[i64]) -> i64 {
         .sum::<i64>()
 }
 
-fn part_two() {}
+fn part_two(xss: &[Vec<i64>], targets: &[i64]) -> i64 {
+    targets
+        .iter()
+        .zip(xss.iter())
+        .map(|(target, xs)| {
+            if dfs(*target, 1, xs, xs[0], false) {
+                return target;
+            }
+            &0
+        })
+        .sum::<i64>()
+}
 
-fn dfs(target: i64, i: usize, matrix: &Vec<i64>, running: i64) -> bool {
+fn concat_vals(mut a: i64, b: i64) -> i64 {
+    if b >= 100 {
+        a *= 1000;
+    } else if b >= 10 {
+        a *= 100;
+    } else {
+        a *= 10;
+    }
+    a + b
+}
+
+fn dfs(target: i64, i: usize, matrix: &Vec<i64>, running: i64, part_one: bool) -> bool {
     if i == matrix.len() && running == target {
         return true;
     }
     if i == matrix.len() || running > target {
         return false;
     }
-    let add = dfs(target, i + 1, matrix, running + matrix[i]);
-    let mul = dfs(target, i + 1, matrix, running * matrix[i]);
-    add || mul
+    let new_con_val = concat_vals(running, matrix[i]);
+    let con = !part_one && dfs(target, i + 1, matrix, new_con_val, part_one);
+    let add = dfs(target, i + 1, matrix, running + matrix[i], part_one);
+    let mul = dfs(target, i + 1, matrix, running * matrix[i], part_one);
+    con || add || mul
 }
 
 pub fn solve() {
@@ -53,5 +77,7 @@ pub fn solve() {
         .map(|(s, _)| s.parse::<i64>().unwrap())
         .collect();
     let part_one_result = part_one(&xss, &targets);
+    let part_two_result = part_two(&xss, &targets);
     println!("{}", part_one_result);
+    println!("{}", part_two_result);
 }
