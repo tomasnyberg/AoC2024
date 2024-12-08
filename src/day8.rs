c2196bafa5112584df2161f0313fcdca404dd84a
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-08 10:31:36 +0100

    day 8: solve part 2

--- src/day8.rs
+++ src/day8.rs
@@ -4,16 +4,31 @@ use std::{
     io::{self, Read},
 };
 
-fn part_one() {}
-
-fn part_two() {}
-
 fn mirror(a: (i32, i32), b: (i32, i32)) -> ((i32, i32), (i32, i32)) {
     let di = b.0 - a.0;
     let dj = b.1 - a.1;
-    let c = (a.0 + 2 * di, a.1 + 2 * dj);
-    let d = (b.0 - 2 * di, b.1 - 2 * dj);
-    (c, d)
+    ((a.0 + 2 * di, a.1 + 2 * dj), (b.0 - 2 * di, b.1 - 2 * dj))
+}
+
+fn resonance_mirror(a: (i32, i32), b: (i32, i32), n: i32, m: i32) -> HashSet<(i32, i32)> {
+    let mut seen: HashSet<(i32, i32)> = HashSet::new();
+    let di = b.0 - a.0;
+    let dj = b.1 - a.1;
+    let mut i = a.0;
+    let mut j = a.1;
+    while i >= 0 && j >= 0 && i < n && j < m {
+        seen.insert((i, j));
+        i += di;
+        j += dj;
+    }
+    i = b.0;
+    j = b.1;
+    while i >= 0 && j >= 0 && i < n && j < m {
+        seen.insert((i, j));
+        i -= di;
+        j -= dj;
+    }
+    seen
 }
 
 pub fn solve() {
@@ -33,16 +48,19 @@ pub fn solve() {
             }
         }
     }
-    let mut seen: HashSet<(i32, i32)> = HashSet::new();
+    let mut seen_p1: HashSet<(i32, i32)> = HashSet::new();
+    let mut seen_p2: HashSet<(i32, i32)> = HashSet::new();
     for (_k, v) in locations.iter() {
         for i in 0..v.len() {
             for j in i + 1..v.len() {
                 let (a, b) = mirror(v[i], v[j]);
-                seen.insert(a);
-                seen.insert(b);
+                seen_p1.insert(a);
+                seen_p1.insert(b);
+                seen_p2.extend(resonance_mirror(v[i], v[j], n, m));
             }
         }
     }
-    seen.retain(|&(i, j)| i >= 0 && j >= 0 && i < n && j < m);
-    println!("{:?}", seen.len());
+    seen_p1.retain(|&(i, j)| i >= 0 && j >= 0 && i < n && j < m);
+    println!("{:?}", seen_p1.len());
+    println!("{:?}", seen_p2.len());
 }
