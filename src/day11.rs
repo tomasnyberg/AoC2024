b51736c887d1f1ce4ffedb6ce49f67964d212237
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-11 09:32:00 +0100

    day 11: refactor out doing one iteration

--- src/day11.rs
+++ src/day11.rs
@@ -9,6 +9,28 @@ pub fn digit_count(mut x: i64) -> i32 {
     result
 }
 
+pub fn one_iter(xs: &mut Vec<i64>) {
+    for i in 0..xs.len() {
+        if xs[i] == 0 {
+            xs[i] = 1;
+            continue;
+        }
+        let dc = digit_count(xs[i]);
+        if dc % 2 == 0 {
+            let mut new_num = 0;
+            let mut pos = 1;
+            for _ in 0..(dc / 2) {
+                new_num += (xs[i] % 10) * pos;
+                pos *= 10;
+                xs[i] /= 10;
+            }
+            xs.push(new_num);
+        } else {
+            xs[i] *= 2024;
+        }
+    }
+}
+
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
@@ -18,25 +40,7 @@ pub fn solve() {
         .map(|l| l.parse().unwrap())
         .collect();
     for _ in 0..25 {
-        for i in 0..input.len() {
-            if input[i] == 0 {
-                input[i] = 1;
-                continue;
-            }
-            let dc = digit_count(input[i]);
-            if dc % 2 == 0 {
-                let mut new_num = 0;
-                let mut pos = 1;
-                for _ in 0..(dc / 2) {
-                    new_num += (input[i] % 10) * pos;
-                    pos *= 10;
-                    input[i] /= 10;
-                }
-                input.push(new_num);
-            } else {
-                input[i] *= 2024;
-            }
-        }
+        one_iter(&mut input);
     }
     println!("{:?}", input.len());
 }
