6e462c3aba49c8fb9ca5877edbdc5b1c511c3b8f
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-12 09:14:32 +0100

    day 12: solve part

--- src/day12.rs
+++ src/day12.rs
@@ -4,6 +4,12 @@ use std::{
 };
 
 const DIRS4: [(i32, i32); 4] = [(1, 0), (0, 1), (0, -1), (-1, 0)];
+const VALID_DIRS: [((i32, i32), (i32, i32)); 4] = [
+    ((0, 1), (0, -1)),
+    ((1, 0), (-1, 0)),
+    ((1, 0), (-1, 0)),
+    ((0, 1), (0, -1)),
+];
 
 pub fn dfs(
     i: i32,
@@ -32,12 +38,34 @@ pub fn dfs(
         .sum::<i32>()
 }
 
+pub fn count_sides(perimeter: &HashSet<(i32, i32, i32)>) -> i32 {
+    let mut sides = 0;
+    let mut visited: HashSet<(i32, i32, i32)> = HashSet::new();
+    for &(i, j, idx) in perimeter.iter() {
+        if visited.contains(&(i, j, idx)) {
+            continue;
+        }
+        let dirs = VALID_DIRS[idx as usize];
+        for (di, dj) in [dirs.0, dirs.1] {
+            let (mut ci, mut cj) = (i + di, j + dj);
+            while perimeter.contains(&(ci, cj, idx)) {
+                visited.insert((ci, cj, idx));
+                ci += di;
+                cj += dj;
+            }
+        }
+        sides += 1;
+    }
+    sides
+}
+
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let lines: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     let mut visited = vec![vec![false; lines[0].len()]; lines.len()];
     let mut result_part_one = 0;
+    let mut result_part_two = 0;
     for i in 0..lines.len() {
         for j in 0..lines[0].len() {
             if !visited[i][j] {
@@ -51,8 +79,10 @@ pub fn solve() {
                     &lines,
                 );
                 result_part_one += perimeter.len() as i32 * node_c;
+                result_part_two += count_sides(&perimeter) * node_c;
             }
         }
     }
     println!("{}", result_part_one);
+    println!("{}", result_part_two);
 }
