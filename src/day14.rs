0539fca00dc0b11a86bd519a5eade540aaa32e30
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-14 11:08:00 +0100

    day 14: count components in loop instead of with dfs

--- src/day14.rs
+++ src/day14.rs
@@ -56,37 +56,6 @@ pub fn score(robots: &[(i32, i32, i32, i32)], n: i32, m: i32) -> i32 {
     scores.iter().cloned().reduce(|a, b| (a * b)).unwrap_or(0)
 }
 
-pub fn dfs(robot_set: &mut Vec<Vec<bool>>, i: i32, j: i32, n: i32, m: i32) {
-    if i < 0 || i >= n || j < 0 || j >= m || !robot_set[i as usize][j as usize] {
-        return;
-    }
-    robot_set[i as usize][j as usize] = false;
-    for &(di, dj) in DIRS8.iter() {
-        dfs(robot_set, i + di, j + dj, n, m);
-    }
-}
-
-pub fn num_components(robots: &[(i32, i32, i32, i32)], n: i32, m: i32) -> i32 {
-    let mut robot_set: Vec<Vec<bool>> = vec![vec![false; m as usize]; n as usize];
-    robots.iter().for_each(|&(x, y, _, _)| {
-        robot_set[y as usize][x as usize] = true;
-    });
-    let mut connected = 0;
-    for i in 0..n {
-        for j in 0..m {
-            if !robot_set[i as usize][j as usize] {
-                continue;
-            }
-            connected += 1;
-            if connected > 200 {
-                return 1000;
-            }
-            dfs(&mut robot_set, i, j, n, m);
-        }
-    }
-    connected
-}
-
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
@@ -101,6 +70,8 @@ pub fn solve() {
     });
     for iter in 0..10000 {
         let mut new_robots: Vec<(i32, i32, i32, i32)> = Vec::new();
+        let mut seen: Vec<Vec<bool>> = vec![vec![false; m as usize]; n as usize];
+        let mut components = 0;
         for &(x, y, dx, dy) in robots.iter() {
             let (mut nx, mut ny) = (x + dx, y + dy);
             if nx < 0 {
@@ -112,12 +83,29 @@ pub fn solve() {
             nx %= m;
             ny %= n;
             new_robots.push((nx, ny, dx, dy));
+            if seen[ny as usize][nx as usize] {
+                continue;
+            } else {
+                seen[ny as usize][nx as usize] = true;
+            }
+            let mut new_c = 1;
+            for &(dx, dy) in DIRS8.iter() {
+                let (ni, nj) = (nx + dx, ny + dy);
+                if ni < 0 || ni >= m || nj < 0 || nj >= n {
+                    continue;
+                }
+                if seen[nj as usize][ni as usize] {
+                    new_c = 0;
+                    break;
+                }
+            }
+            components += new_c;
         }
         robots = new_robots;
         if iter == 99 {
             println!("{}", score(&robots, n, m));
         }
-        if num_components(&robots, n, m) < 200 {
+        if components < 250 {
             println!("{}", iter + 1);
             break;
         }
