b70ac5260f68ce13f3fd3b4046afce556b65a641
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-19 09:17:16 +0100

    day 19: solve part 2

--- src/day19.rs
+++ src/day19.rs
@@ -28,29 +28,28 @@ fn search(
     mut node: &TrieNode,
     target: &Vec<char>,
     i: usize,
-    seen: &mut HashMap<usize, bool>,
-) -> bool {
+    seen: &mut HashMap<usize, i64>,
+) -> i64 {
     if i == target.len() {
-        return true;
+        return 1;
     }
     if seen.contains_key(&i) {
         return seen[&i];
     }
+    let mut result = 0;
     for j in i..target.len() {
         if node.children[target[j] as usize - 'a' as usize].is_none() {
-            seen.insert(i, false);
-            return false;
+            break;
         }
         node = node.children[target[j] as usize - 'a' as usize]
             .as_ref()
             .unwrap();
-        if node.is_word && search(root, root, target, j + 1, seen) {
-            seen.insert(i, true);
-            return true;
+        if node.is_word {
+            result += search(root, root, target, j + 1, seen);
         }
     }
-    seen.insert(i, false);
-    false
+    seen.insert(i, result);
+    result
 }
 
 pub fn solve() {
@@ -74,9 +73,8 @@ pub fn solve() {
     let mut result = 0;
     let mut seen = HashMap::new();
     for target in targets {
-        if search(&root, &root, &target, 0, &mut seen) {
-            result += 1;
-        }
+        let add = search(&root, &root, &target, 0, &mut seen);
+        result += add;
         seen.clear();
     }
     println!("{}", result);
