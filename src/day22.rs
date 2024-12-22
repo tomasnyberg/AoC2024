7ac0549e19769df56bd10e5ac765aa7dc1c540b4
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-22 09:41:09 +0100

    day 22: integer keys instead of tuples and deques

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, HashSet, VecDeque},
+    collections::{HashMap, HashSet},
     io::{self, Read},
 };
 
@@ -13,27 +13,22 @@ pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let nums: Vec<i64> = input.lines().map(|line| line.parse().unwrap()).collect();
-    let mut totals: HashMap<(i64, i64, i64, i64), i64> = HashMap::new();
+    let mut totals: HashMap<i64, i64> = HashMap::new();
     let mut part_one = 0;
     for num in nums {
         let mut curr = num;
         let mut prev_ones = curr % 10;
-        let mut dq: VecDeque<i64> = VecDeque::new();
-        let mut seen: HashSet<(i64, i64, i64, i64)> = HashSet::new();
-        let mut curr_key = (-11, -11, -11, -11);
-        for _ in 0..2000 {
+        let mut seen: HashSet<i64> = HashSet::new();
+        let mut curr_key = 0;
+        for i in 0..2000 {
             curr = evolve(curr);
             let ones = curr % 10;
             let diff = ones - prev_ones;
+            curr_key <<= 5;
+            curr_key += diff;
+            curr_key &= (1 << 20) - 1;
             prev_ones = ones;
-            dq.push_back(diff);
-            if dq.len() > 4 {
-                dq.pop_front();
-            }
-            if dq.len() == 4 {
-                curr_key = (dq[0], dq[1], dq[2], dq[3]);
-            }
-            if curr_key != (-11, -11, -11, -11) && !seen.contains(&curr_key) {
+            if i > 3 && !seen.contains(&curr_key) {
                 totals.insert(curr_key, *totals.get(&curr_key).unwrap_or(&0) + ones);
                 seen.insert(curr_key);
             }
