c0ce1ed1e7dfaefcc448c76103bec05238810c70
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-29 10:17:59 +0100

    day 25: faster and cleaner solution using bits

--- src/day25.rs
+++ src/day25.rs
@@ -1,67 +1,25 @@
 use std::io::{self, Read};
 
-fn check(key: Vec<usize>, lock: Vec<usize>) -> bool {
-    for i in 0..key.len() {
-        if key[i] + lock[i] > 5 {
-            return false;
-        }
-    }
-    true
-}
-
 pub fn solve() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let parts: Vec<&str> = input.split("\n\n").collect();
-    let mut grids: Vec<Vec<Vec<char>>> = Vec::new();
-    parts.iter().for_each(|part| {
-        let mut lock: Vec<Vec<char>> = Vec::new();
-        part.lines().for_each(|line| {
-            let mut lock_line: Vec<char> = Vec::new();
-            line.chars().for_each(|c| {
-                lock_line.push(c);
-            });
-            lock.push(lock_line);
-        });
-        grids.push(lock);
-    });
-
-    let mut locks: Vec<Vec<usize>> = Vec::new();
-    for grid in grids.iter() {
-        if grid[0][0] == '#' {
-            let mut lock: Vec<usize> = Vec::new();
-            for c in 0..grid[0].len() {
-                let mut count = 0;
-                for r in 1..grid.len() {
-                    if grid[r][c] != '#' {
-                        break;
-                    }
-                    count += 1;
-                }
-                lock.push(count);
-            }
-            locks.push(lock);
+    let mut locks: Vec<u32> = Vec::new();
+    let mut keys: Vec<u32> = Vec::new();
+    for p in parts.iter() {
+        let bits = p.as_bytes()[6..35]
+            .iter()
+            .fold(0, |acc, &b| acc << 1 | (b == b'#') as u32);
+        if p.starts_with('#') {
+            locks.push(bits);
+        } else {
+            keys.push(bits);
         }
     }
     let mut result = 0;
-    for grid in grids.iter() {
-        if grid[0][0] == '.' {
-            let mut key: Vec<usize> = Vec::new();
-            for c in 0..grid[0].len() {
-                let mut count = 0;
-                for r in (1..grid.len() - 1).rev() {
-                    if grid[r][c] != '#' {
-                        break;
-                    }
-                    count += 1;
-                }
-                key.push(count);
-            }
-            for lock in locks.iter() {
-                if check(key.clone(), lock.clone()) {
-                    result += 1;
-                }
-            }
+    for lock in &locks {
+        for key in &keys {
+            result += (lock & key == 0) as u32;
         }
     }
     println!("{}", result);
